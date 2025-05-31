fc0ae1ae9c81c1a0879adcb1a78b8991699fdd30
wakamonoo <joven.serdanbataller21@gmail.com>
2025-06-01 00:06:48 +0800

    transferred firebase

--- js/config.js
+++ js/config.js
@@ -1,10 +1,10 @@
 export const firebaseConfig = {
-  apiKey: "AIzaSyArg_0FQXtdEO_sVL3SpqddyROk6VGPtkM",
-  authDomain: "tsunagari-665d3.firebaseapp.com",
-  projectId: "tsunagari-665d3",
-  storageBucket: "tsunagari-665d3.firebasestorage.app",
-  messagingSenderId: "724015647050",
-  appId: "1:724015647050:web:8ae0b1ec0339f8689199b3",
-  measurementId: "G-N1LLZZHRMW"
+  apiKey: "AIzaSyCSVxEeW_5paPYO10pWiRxg3NYuk8_nBao",
+  authDomain: "kandili-92588.firebaseapp.com",
+  projectId: "kandili-92588",
+  storageBucket: "kandili-92588.firebasestorage.app",
+  messagingSenderId: "493025517878",
+  appId: "1:493025517878:web:497a36baf717419d73d397",
+  measurementId: "G-SNK9GJNWJR",
 };
 export const IMGUR_CLIENT_ID = "dd7b011c325aaa7";
